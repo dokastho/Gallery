59d935ffdf77239976ac839ca1af4c16e12e1199
Thomas Dokas <tjdokas@gmail.com>
2023-09-07 00:29:36 +0000

    update from dokasfam

--- gallery/common/utils.py
+++ gallery/common/utils.py
@@ -7,11 +7,11 @@ import gallery
 
 def get_client(host_id: int = 0) -> d3b_client:
     # if "FLASK_DEBUG" in os.environ.keys():
-    #     return d3b_client("https://dev2.dokasfam.com")
+    #     return d3b_client("https://dev2.dokastho.io")
     if host_id == 0:
         host_id = randint(1, gallery.app.config["NDBS"])
         pass
     
-    hostname = f'https://d3b{host_id}.dokasfam.com'
+    hostname = f'https://d3b{host_id}.dokastho.io'
     c = d3b_client(hostname)
     return c
